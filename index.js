3c42002511ef9e8fd479cedba4660cf321ff4f7e
haoting <sssssrrt01@gmail.com>
2023-08-16 19:11:52 -0700

    First complete production version Notes: - rate of fetchProducts limited directly to avoid connection refusal from web host - JSON of scraped data can be accessed via data.json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const PORT = 8000;
+const fs = require('fs');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
@@ -8,8 +9,8 @@ const app = express();
 const mainUrl = 'https://www.abercrombie.com/shop/us/mens';
 
 const foundUrls = [];
-const products = [];
 
+const filePath = 'data.json';
 async function fetchUrls() {
     try {
         const response = await axios(mainUrl);
@@ -17,27 +18,36 @@ async function fetchUrls() {
         const $ = cheerio.load(html);
         const pageCount = $('.page-indicator div[aria-live="assertive"]').text().trim().split(' ')[2];
 
+        const urlFetchPromises = [];
         for (let i = 1; i <= pageCount; i++) {
             try {
-                const response = await axios(`${mainUrl}?filtered=true&rows=90&start=${(i * 90) - 90}`);
-                const html = response.data;
-                const $ = cheerio.load(html);
-
-                $('.catalog-productCard-module__productCard').each(function () {
-                    const href = $(this).find('a').attr('href');
-                    foundUrls.push(href);
-                });
+                const promise = axios(`${mainUrl}?filtered=true&rows=90&start=${(i * 90) - 90}`);
+                urlFetchPromises.push(promise);
             } catch (err) {
                 console.log(err);
             }
         }
-        console.log(foundUrls)
+
+        const urlResponses = await Promise.all(urlFetchPromises);
+
+        urlResponses.forEach(response => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+
+            $('.catalog-productCard-module__productCard').each(function () {
+                const href = $(this).find('a').attr('href');
+                foundUrls.push(href);
+            });
+        });
+
+        console.log(foundUrls);
     } catch (err) {
         console.log(err);
     }
 }
 
 async function fetchProducts() {
+    let isFirst = true;
     for (const urlSuffix of foundUrls) {
         try {
             const itemLink = `https://www.abercrombie.com${urlSuffix}`
@@ -52,11 +62,25 @@ async function fetchProducts() {
 
                 const productPrice = $(this).find('.product-price-text-wrapper').text().trim().split('$');
 
-                products.push({
+                const productInfo = {
                     Item: productName,
                     Link: productURL,
                     Price: productPrice[productPrice.length - 1]
-                });
+                };
+
+                const jsonData = JSON.stringify(productInfo, null, 2);
+                try {
+                    if (!isFirst) {
+                        fs.appendFileSync(filePath, ',');
+                    }
+                    fs.appendFileSync(filePath, jsonData + '\n');
+
+                    isFirst = false;
+
+                    console.log(jsonData + '\n');
+                } catch (error) {
+                    console.error(`Error appending to ${filePath}: `, error);
+                }
             });
         } catch (err) {
             console.log(err);
@@ -65,9 +89,14 @@ async function fetchProducts() {
 }
 
 async function startApp() {
+
+    fs.writeFileSync(filePath, '[');
+
     await fetchUrls();
     await fetchProducts();
-    console.log(products)
+
+    fs.appendFileSync(filePath, ']');
+
 
     app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 }
