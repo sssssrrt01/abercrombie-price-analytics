138d16cabeeaa42fbc038926620e4aff99e3a710
haoting <sssssrrt01@gmail.com>
2023-08-18 00:08:55 -0700

    added pandas check for lowest and average price

--- data-analyzer.py
+++ data-analyzer.py
@@ -29,6 +29,19 @@ for elt in cur.description:
 df = pd.DataFrame(data=data, columns=cols)
 df = df.set_index('id')
 
+average_price = df.groupby('link').agg(
+    item_name=('item', 'first'),  # Preserve the item_name
+    mean_price=('price', 'mean')       # Calculate the mean of prices
+)
+
+lowest_price = df.groupby('link').agg(
+    item_name=('item', 'first'),  # Preserve the item_name
+    lowest_price=('price', 'min')       # Calculate the min of prices
+)
+
+
 print(df)
+print(average_price)
+print(lowest_price)
 
 con.close()
